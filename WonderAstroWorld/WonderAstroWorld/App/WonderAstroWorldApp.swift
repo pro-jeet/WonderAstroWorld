c2f89fd078268423bd402854b3fee3bd1764ac06
Jitesh Sharma <sharma31121988@gmail.com>
2024-05-27 00:03:45 +0530

    WonderAstroWorld  Version1.0 Build 1 - Project Setup - Initial Commit Deployment targets - iOS (17.5), macOS (14.4)

---
+++ WonderAstroWorld/WonderAstroWorld/App/WonderAstroWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  WonderAstroWorldApp.swift
+//  WonderAstroWorld
+//
+//  Created by JSharma on 26/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WonderAstroWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WonderAstroWorld/WonderAstroWorld/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WonderAstroWorld
+//
+//  Created by JSharma on 26/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+//#Preview {
+//    ContentView()
+//}
